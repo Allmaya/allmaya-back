0cc09305afd2bfdcd35ba288d8b143145b525fa1
박홍진 <parkhongjin@baghongjin-ui-MacBookPro.local>
2022-01-04 22:28:57 +0900

    * 인증 REST API 구조 작성

---
+++ src/api/auth/index.js
@@ -0,0 +1,7 @@
+const Router = require('koa-router');
+
+const auth = new Router();
+
+// Auth REST API Route
+
+module.exports = auth;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 
+const api = require('./api');
+
 const app = new Koa();
+const router = new Router();
 
 const { PORT } = process.env;
 
+router.use('/api', api.routes());
+
 app.use(bodyParser());
+
+app.use(router.routes()).use(router.allowedMethods());
 
 const port = PORT || 4000;
 app.listen(port, () => {
