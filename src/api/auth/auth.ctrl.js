20020c07280000f21926e9dd99840241098d094a
박홍진 <parkhongjin@baghongjin-ui-MacBookPro.local>
2022-01-04 23:40:27 +0900

    * 인증관리를 위한 API 구성 인터페이스 구성 완료

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -1,18 +1,49 @@
 
+/*
+    POST /api/auth/register
+    {
+        sns_token: ''
+    }
+*/
 const register = async (ctx) => {
-    ctx.body = 'register';
+    const { sns_token } = ctx.request.body;
+
+    ctx.body = `register - not implemented! received : ${sns_token}`;
+    ctx.cookies.set('access_token', 'register - not implemented!', {
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+        httpOnly: true,
+    });
 };
 
+/*
+    POST /api/auth/login
+    {
+        sns_token: ''
+    }
+*/
 const login = async (ctx) => {
-    ctx.body = 'login';
+    const { sns_token } = ctx.request.body;
+
+    ctx.body = `login - not implemented! received : ${sns_token}`;
+    ctx.cookies.set('access_token', 'login - not implemented!', {
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+        httpOnly: true,
+    });
 };
 
+/*
+    GET /api/auth/check
+*/
 const check = async (ctx) => {
-    ctx.body = 'check';
+    ctx.body = 'check - not implemented!';
 };
 
+/*
+    POST /api/auth/logout
+*/
 const logout = async (ctx) => {
-    ctx.body = 'logout';
+    ctx.body = 'loout - not implemented!';
+    ctx.status = 204; // No Content
 };
 
 module.exports = { register, login, check, logout };
