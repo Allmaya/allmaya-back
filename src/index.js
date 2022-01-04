0ee6d24bff1077aae9bc28f9780745ed4bd3295a
박홍진 <parkhongjin@baghongjin-ui-MacBookPro.local>
2022-01-04 22:17:39 +0900

    * 모듈추가 : router, bodyparser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 
 const { PORT } = process.env;
+
+app.use(bodyParser());
 
 const port = PORT || 4000;
 app.listen(port, () => {
