e071dd11bb9a0b421790def11a972566f402756b
starcush <hacker0419@gmail.com>
2022-03-23 17:19:12 +0900

    update
     - add status message
     - install nodemon

--- src/lib/response.js
+++ src/lib/response.js
@@ -1,32 +1,33 @@
-const STATUS = require("./status");
+const STATUS = require('./status');
 
 const setBasicResponse = async (ctx, statusCode, message) => {
-    let statusMsg;
-    switch (statusCode) {
-        case STATUS.STATUS_OK:
-            statusMsg = STATUS.STATUS_MSG_OK;
-            break;
-        case STATUS.STATUS_OK_NO_CONTENT:
-            statusMsg = STATUS.STATUS_MSG_OK_NO_CONTENT;
-            break;
-        case STATUS.STATUS_BAD_REQUEST:
-            statusMsg = STATUS.STATUS_MSG_BAD_REQUEST;
-            break;
-        case STATUS.STATUS_UNAUTHORIZED:
-            statusMsg = STATUS.STATUS_MSG_UNAUTHORIZED;
-            break;
-        case STATUS.STATUS_UNKNOWN:
-            statusMsg = STATUS.STATUS_MSG_UNKNOWN;
-            break;
-        default: 
-            statusMsg = STATUS.STATUS_MSG_UNKNOWN;
-    }
+  let statusMsg;
+  switch (statusCode) {
+    case STATUS.STATUS_OK:
+      statusMsg = STATUS.STATUS_MSG_OK;
+      break;
+    case STATUS.STATUS_OK_NO_CONTENT:
+      statusMsg = STATUS.STATUS_MSG_OK_NO_CONTENT;
+      break;
+    case STATUS.STATUS_BAD_REQUEST:
+      statusMsg = STATUS.STATUS_MSG_BAD_REQUEST;
+      break;
+    case STATUS.STATUS_UNAUTHORIZED:
+      statusMsg = STATUS.STATUS_MSG_UNAUTHORIZED;
+      break;
+    case STATUS.STATUS_UNKNOWN:
+      statusMsg = STATUS.STATUS_MSG_UNKNOWN;
+      break;
+    default:
+      statusMsg = STATUS.STATUS_MSG_UNKNOWN;
+  }
 
-    ctx.status = statusCode;
-    ctx.body = { 
-        status: statusCode,
-        message: `[${statusMsg}] ${message}`,
-    };
-}
+  ctx.status = statusCode;
+  ctx.body = {
+    status: statusCode,
+    message: `[${statusMsg}] ${message}`,
+  };
+  console.log(`[${statusCode} - ${statusMsg}] ${message}`);
+};
 
 module.exports = { setBasicResponse };
